7493cc83bdd066ab69878457fffb30d6bda079e0
lvheyang <cuiwenzheng@ymail.com>
2021-06-17 14:03:15 +0800

    fix

--- datafusion/benches/math_query_sql.rs
+++ datafusion/benches/math_query_sql.rs
@@ -76,7 +76,7 @@ fn create_context(
 
 fn criterion_benchmark(c: &mut Criterion) {
     let array_len = 6000001; // 2^20
-    let batch_size = 4096; // 2^9
+    let batch_size = 9192; // 2^9
     let ctx = create_context(array_len, batch_size).unwrap();
     c.bench_function("add_2_column_sql", |b| {
         b.iter(|| query(ctx.clone(), "SELECT f32+f64 FROM t"))
